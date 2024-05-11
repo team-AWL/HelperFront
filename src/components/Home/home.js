d0975a7df1c91fbb321277fe42ae617576367686
bogdan9876 <bogdankryveiko@gmail.com>
2024-05-11 22:47:03 +0300

    next commit

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -3,16 +3,23 @@ import Header from '../Header/header';
 import Footer from '../Footer/footer';
 import Section1 from './Section1/section1';
 import Section2 from './Section2/section2';
+import ItemHelp from '../Search/ItemHelp/itemHelp'
+import './home.css'
 
 const Home = () => {
-    return (
-        <>
-          <Header />
-          <Section1 />
-          <Section2 />
-          <Footer />
-        </>
-      );
+  return (
+    <>
+      <Header />
+      <Section1 />
+      <Section2 />
+      <div className="newsHeader">
+        <h1 className="newsTitle">Терміновий пошук</h1>
+        <p className="allNewsLink">Переглянути більше</p>
+      </div>
+      <ItemHelp />
+      <Footer />
+    </>
+  );
 }
 
 export default Home
