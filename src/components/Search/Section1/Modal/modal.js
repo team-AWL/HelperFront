e81817cc632bc71bf93b86d71dffe974b294a74b
bogdan9876 <bogdankryveiko@gmail.com>
2024-05-12 07:44:30 +0300

    fix bug

--- src/components/Search/Section1/Modal/modal.js
+++ src/components/Search/Section1/Modal/modal.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './modal.css';
-import {createAnnouncement, login} from "../../../../api";
+import { createAnnouncement, login } from "../../../../api";
 
 const Modal = ({ handleCloseModal }) => {
     const [formData, setFormDataFundraising] = useState({
@@ -46,70 +46,65 @@ const Modal = ({ handleCloseModal }) => {
             <h2 className="title5">Потребуєш допомоги в пошуці зниклого?</h2>
             <p className="dataInput">Введи дані:</p>
             <form onSubmit={handleSubmit}>
-            <div className="inputContainer">
-                <div className='modal-first-section'>
-                    <div className='modal-4inputs'>
-                        <input
-                            type="text"
-                            name="fullNameOfPerson"
-                            value={formData.fullNameOfPerson}
-                            placeholder="Ім'я, Прізвище"
-                            className="input"
-                            onChange={handleInputChange}
-                        />
-                        <input
-                            type="text"
-                            name='location'
-                            value={formData.location}
-                            placeholder="Локація"
-                            className="input"
-                            onChange={handleInputChange}
-                        />
-                        <input
-                            type="text"
-                            name='seemLastTime'
-                            value={formData.seemLastTime}
-                            placeholder="Дата останнього зв'язку(день.місяць.рік)"
-                            className="input"
-                            onChange={handleInputChange}
-                        />
-                        <input
-                            type="text"
-                            name='imageUrl'
-                            value={formData.imageUrl}
-                            placeholder="Введіть силку на фотографію"
-                            className="input"
-                            onChange={handleInputChange}
-                        />
-                        <input
-                            type="text"
-                            name='contactInformation'
-                            value={formData.contactInformation}
-                            placeholder="Спосіб зв'язку"
-                            className="input"
-                            onChange={handleInputChange}
-                        />
-                    </div>
+                <div className="inputContainer">
+                    <div className='modal-first-section'>
+                        <div className='modal-4inputs'>
+                            <input
+                                type="text"
+                                name="fullNameOfPerson"
+                                value={formData.fullNameOfPerson}
+                                placeholder="Ім'я, Прізвище"
+                                className="input"
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                type="text"
+                                name='location'
+                                value={formData.location}
+                                placeholder="Локація"
+                                className="input"
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                type="text"
+                                name='seemLastTime'
+                                value={formData.seemLastTime}
+                                placeholder="Дата останнього зв'язку(день.місяць.рік)"
+                                className="input"
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                type="text"
+                                name='imageUrl'
+                                value={formData.imageUrl}
+                                placeholder="Введіть силку на фотографію"
+                                className="input"
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                type="text"
+                                name='contactInformation'
+                                value={formData.contactInformation}
+                                placeholder="Спосіб зв'язку"
+                                className="input"
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                type="text"
+                                name='description'
+                                value={formData.contactInformation}
+                                placeholder="Опис"
+                                className="input"
+                                onChange={handleInputChange}
+                            />
 
+                        </div>
+
+                    </div>
                 </div>
-                <input
-                    type="text"
-                    name='description'
-                    value={formData.description}
-                    placeholder="Опис"
-                    className="fullWidthInput"
-                    onChange={handleInputChange}
-                    style={{
-                        width: '90%',
-                        marginLeft: '2rem',
-                        marginBottom: '4rem',
-                        height: '100%'
-                    }}
-                />
-            </div>
-            <button className="registerButton5">
-                Зареєструвати
-            </button>
+                <button className="registerButton5">
+                    Зареєструвати
+                </button>
             </form>
         </div>
     );
